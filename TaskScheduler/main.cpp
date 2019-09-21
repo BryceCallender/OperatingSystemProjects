052901e4669b9852aa7a0815bc3cca3175573ec3
Bryce <brycec848@gmail.com>
2019-09-21 15:54:27 -0700

    added comments and fixed it from RR-3 to RR-5

--- TaskScheduler/main.cpp
+++ TaskScheduler/main.cpp
@@ -23,35 +23,40 @@ void printScheduleTable(const std::vector<Job>& jobs);
 
 int main()
 {
-	std::cout << "The average turn around time is: " << firstComeFirstServedAlgorithm() << std::endl << std::endl;
+	std::cout << "\nThe average turn around time is: " << firstComeFirstServedAlgorithm() << std::endl << std::endl;
 
-	std::cout << "The average turn around time is: " << shortestJobFirst() << std::endl << std::endl;
+	std::cout << "\nThe average turn around time is: " << shortestJobFirst() << std::endl << std::endl;
 
-	std::cout << "The average turn around time is: " << roundRobin(2) << std::endl << std::endl;
+	std::cout << "\nThe average turn around time is: " << roundRobin(2) << std::endl << std::endl;
 
-	std::cout << "The average turn around time is: " << roundRobin(3) << std::endl << std::endl;
-
+	std::cout << "\nThe average turn around time is: " << roundRobin(5) << std::endl << std::endl;
+	
 	return 0;
 }
 
 void readJobFile(std::vector<Job>& jobs)
 {
-	std::ifstream inputFile("/Users/brycecallender/CLionProjects/TaskScheduler/job.txt");
+	std::ifstream inputFile("job.txt");
 	
 	while (!inputFile.eof())
 	{
+		//Read in the job name
 		std::string name;
 		std::getline(inputFile, name);
 
+		//Read in the burst time
 		int burstTime;
 		inputFile >> burstTime;
 
+		//Clean input buffer
 		inputFile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
+		//Make the job
 		Job job;
 		job.name = name;
 		job.burstTime = burstTime;
 
+		//Add it to the vector
 		jobs.push_back(job);
 	}
 
@@ -60,10 +65,14 @@ void readJobFile(std::vector<Job>& jobs)
 
 void printScheduleTable(const std::vector<Job>& jobs)
 {
+	//Prints out the table headers
 	std::cout << std::left << std::setw(12) << "Job Name" << std::setw(14) << "Start Time" << std::setw(12) << "End Time" << std::setw(19) << "Job Completion" << std::endl;
+	//For each job in the job list
 	for(const Job& job : jobs)
 	{
+		//Print out thye name, starting time, end time
 		std::cout << std::left << std::setw(12) << job.name << std::setw(14) << job.startTime << std::setw(12) << job.endTime;
+		//If the job is completed print out that it completed @end time
 		if(job.burstTime == 0)
         {
             std::cout << std::setw(19) << job.name + " completed at @" + std::to_string(job.endTime);
@@ -74,35 +83,48 @@ void printScheduleTable(const std::vector<Job>& jobs)
 
 double firstComeFirstServedAlgorithm()
 {
+	std::cout << "Performing First Come First Serve Algorithm!\n";
+	
 	std::vector<Job> jobs;
 
+	//Read the file to get the job information
 	readJobFile(jobs);
 
 	int timer = 0;
 	double averageTurnAroundTime = 0.0;
 
+	//For each job in the list
 	for (Job& currentJob : jobs)
 	{
+		//Make the job's start time based on the current timer
         currentJob.startTime = timer;
 
+		//Add the burst time to the timer and retire the job
 		timer += currentJob.burstTime;
 		currentJob.burstTime = 0;
 
+		//End the job at this time and add the time to the averageTimer
 		currentJob.endTime = timer;
 		averageTurnAroundTime += currentJob.endTime;
 	}
 
+	//Print the Schedule Table
 	printScheduleTable(jobs);
-	
+
+	//Return the average time
 	return averageTurnAroundTime / jobs.size();
 }
 
 double shortestJobFirst()
 {
+	std::cout << "Performing Shortest Job First Algorithm!\n";
+	
 	std::vector<Job> jobs;
 
+	//Read the file to get the job information
 	readJobFile(jobs);
 
+	//Sort the jobs by their burst time so the shortest job is at the beginning 
 	std::sort(jobs.begin(), jobs.end(), [](Job a, Job b)
 	{
 		return a.burstTime < b.burstTime;
@@ -111,75 +133,95 @@ double shortestJobFirst()
 	int timer = 0;
 	double averageTurnAroundTime = 0.0;
 
+	//For each job in the list
 	for (Job& currentJob : jobs)
 	{
-        currentJob.startTime = timer;
+		//Make the job's start time based on the current timer
+		currentJob.startTime = timer;
 
+		//Add the burst time to the timer and retire the job
 		timer += currentJob.burstTime;
-        currentJob.burstTime = 0;
+		currentJob.burstTime = 0;
 
+		//End the job at this time and add the time to the averageTimer
 		currentJob.endTime = timer;
 		averageTurnAroundTime += currentJob.endTime;
 	}
 
+	//Print the Schedule Table
 	printScheduleTable(jobs);
 
+	//Return the average time
 	return averageTurnAroundTime / jobs.size();
 	
 }
 
 double roundRobin(int roundRobinSlice)
 {
+	std::cout << "Performing Round Robin with time slice " + std::to_string(roundRobinSlice) + " or RR-" + std::to_string(roundRobinSlice) << std::endl;
+	
 	std::vector<Job> jobs;
 
+	//Read the jobs from the file
 	readJobFile(jobs);
 
+	//Make a copy of the job list so we can keep adding to it
 	std::vector<Job> jobList = jobs;
 
 	int timer = 0;
-	int jobSize = jobs.size();
 	double averageTurnAroundTime = 0.0;
+	int tasksRemoved = 0;
 	int index = 0;
 
-    while(!jobs.empty())
+	//While we have tasks to be done still
+    while(tasksRemoved != jobs.size())
     {
+    	//Get the job at the current index of the list and set its starting time
         Job& currentJob = jobList[index];
         currentJob.startTime = timer;
 
         //Does the current job not require all of the round robin time?
         if(currentJob.burstTime - roundRobinSlice < 0)
         {
+        	//Add to the timer the burst time we have left
             timer += currentJob.burstTime;
-            //Get rid of the correct job based on index we are (circularly)
+        	//Set the ending time and retire the job
             currentJob.endTime = timer;
             currentJob.burstTime = 0;
+        	//Add to the average time and increment since a job was removed
             averageTurnAroundTime += currentJob.endTime;
-            jobs.erase(jobs.begin() + (index % jobSize));
+			tasksRemoved++;
         }
         //Does it take all the time?
         else if(currentJob.burstTime - roundRobinSlice == 0)
         {
+        	//Add the slice time to the timer since it used all the time
             timer += roundRobinSlice;
+			//Retire the job and set its ending time
             currentJob.burstTime = 0;
             currentJob.endTime = timer;
+			//Add to the average time and increment since a job was removed
             averageTurnAroundTime += currentJob.endTime;
-            //jobList.push_back(currentJob);
-            //Get rid of the front of the list of jobs
-            jobs.erase(jobs.begin() + (index % jobSize));
+			tasksRemoved++;
         }
         //Does it have time left?
         else
         {
+        	//Increment timer since it used all the time
             timer += roundRobinSlice;
+        	//Take away the burst time by slice amount
             currentJob.burstTime -= roundRobinSlice;
+        	//The job will be switched out so set the end time of the job to right now and add it to the back of the list
             currentJob.endTime = timer;
             jobList.push_back(currentJob);
         }
+    	//Next job
         index++;
     }
 
+	//Print the Schedule Table
     printScheduleTable(jobList);
 
-    return averageTurnAroundTime / jobSize;
-	
+	//Return the average time
+    return averageTurnAroundTime / jobs.size();
 }
